df0ab1e29624315357ca5621d1d5d9a1dd4ae1ab
Manish Tiwari <manishdoestech@gmail.com>
2024-09-11 21:18:01 +0530

    Time: 37 ms (56.39%), Space: 16.5 MB (23.03%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,9 +1,12 @@
 import operator as op
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        index = 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[index] = nums[i]
-                index += 1
-        return index
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            if nums[left] == val:
+                nums[left] = nums[right]
+                right -= 1
+            else:
+                left += 1
+        return left
