86dc36ea797f18a9bd6a2e9bbf51243d871407ec
Manish Tiwari <manishdoestech@gmail.com>
2026-01-12 00:49:20 +0530

    Time: 20 ms (10.56%), Space: 46.6 MB (17.36%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,16 +2,10 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-    public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
-            return false;
-        }
-        Map<Character, Integer> result = new HashMap<>();
-        for (int i = 0; i < s.length(); i++) {
-            result.merge(s.charAt(i), 1, Integer::sum);
-            result.merge(t.charAt(i), -1, Integer::sum);
-        }
-        // Return true only if all counts cancel out to zero
-        return result.values().stream().allMatch(v -> v == 0);
+    public boolean isAnagram(String s, String t) {  
+return Arrays.equals(
+        s.chars().sorted().toArray(), 
+        t.chars().sorted().toArray()
+    );        
     }
 }
